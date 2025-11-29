9bed6da608ca57a05c7ee26a07afab352c96ab2d
Russell Foo <iruss1337@gmail.com>
2025-11-29 15:46:52 +0800

    feat: add ft_atoi to convert char to int

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*   By: russ1337 <russ1337@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 22:48:21 by rfoo              #+#    #+#             */
-/*   Updated: 2025/11/28 22:58:49 by rfoo             ###   ########.fr       */
+/*   Updated: 2025/11/29 15:46:12 by russ1337         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 
 int	ft_atoi(const char *nptr)
 {
-	if (*nptr == "-2147483648")
-		return (-2147483648);
+	int	i;
+	int	sign;
+	int	num;
 
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		num = num * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (sign * num);
 }
